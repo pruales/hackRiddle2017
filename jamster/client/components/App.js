743faadb177c47cc225a28d9fb074ac901289313
Paul Ruales <paul-ruales@uiowa.edu>
2017-11-11 15:53:47 -0500

    set up

---
+++ jamster/client/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+/**
+ * Main app component
+ * Has a header and then render's the page content
+ */
+export default class SpotifyLogin extends Component {
+  render() {
+    // injected via react router
+    const {children} = this.props;
+    return (
+      <div className="spotify-login">
+        <h1>Our Aux</h1>
+        <div className="page-content">
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
